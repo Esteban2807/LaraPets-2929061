e57898748ef867067839cf29526712e8d8c4a94a
Esteban Muñoz <juanmua2007@gmail.com>
2026-05-08 18:26:31 -0500

    scripts editedes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,29 +1,34 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title')</title>
-    <link href="{{ asset('css/daisyui5.css') }}" rel="stylesheet" type="text/css"/>
+    <link href="{{ asset('css/daisyui5.css') }}" rel="stylesheet" type="text/css" />
+    <link href="https://cdn.jsdelivr.net/npm/daisyui@5" rel="stylesheet" type="text/css" />
     <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
 </head>
 @auth
-@php
-(Auth::user()->role == 'Customer') ? $colors = 'blue,bluesky' : $colors = 'green,lime'
-@endphp
+    @php
+        (Auth::user()->role == 'Customer') ? $colors = 'blue,bluesky' : $colors = 'green,lime'
+    @endphp
 @else
     @php
         $colors = 'black,transparent'
     @endphp
 @endauth
-<body class="bg-[linear-gradient(to_top,{{ $colors }})] bg-[url({{asset('images/backgroundd.jpg')}})] min-h-dvh bg-no-repeat bg-center bg-cover pt-14 bg-fixed">
+
+<body
+    class="bg-[linear-gradient(to_top,{{ $colors }})] bg-[url({{asset('images/backgroundd.jpg')}})] min-h-dvh bg-no-repeat bg-center bg-cover pt-14 bg-fixed">
     <main class="p-12 flex flex-col gap-2 justify-center items-center min-h-dvh">
-        
-            @yield('content')
+
+        @yield('content')
     </main>
-        <script src="{{ asset('js/jquery-3.7.1.min.js') }}"></script>
-        <script src="{{ asset('js/sweetalert2@11.js') }}"></script>
-        @yield('js')
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <script src="https://cdn.jsdelivr.net/npm/daisyui@5"></script>
+    @yield('js')
 </body>
 
 </html>
